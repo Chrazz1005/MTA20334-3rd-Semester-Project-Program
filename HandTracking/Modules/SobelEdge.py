11e9dbee7792d31528488d1f3ee591363206918d
Sebastian <sebber280@live.dk>
2020-10-20 11:54:53 +0200

    added modules + SobelEdge

---
+++ HandTracking/Modules/SobelEdge.py
@@ -0,0 +1,37 @@
+
+import cv2
+import numpy as np
+
+class SobelEdge:
+    __img = 0
+    gxKernelArray = np.array(([-1, 0, 1], [-2, 0, 2], [-1, 0, 1]), np.float32)
+    gyKernelArray = np.array(([1, 2, 1], [0, 0, 0], [-1, -2, -1]), np.float32)
+
+
+    def __init__(self, img):
+        self.__img = img
+
+
+    def detectionLoop(self):
+        n, m, d = self.__img.shape
+
+        edge_img = np.zeros_like(self.__img)
+
+        for row in range(3,
+                         n - 2):  # It is  above the range 0 to end, because we ignore the edges of the image since the kernel might not fit
+            for col in range(3, m - 2):
+                local_pixels = self.__img[row - 1:row + 2, col - 1:col + 2, 0]  # defining 3x3 box for checking the img
+
+                trans_pixels = self.gyKernelArray * local_pixels  # multiply box with vertical kernel array
+                vert_score = trans_pixels.sum() / 4  ##ift. +4 og /8 - Our kernel returns a value between -4 and 4, we want to map the from 0-1.
+
+                hori_pixels = self.gxKernelArray * local_pixels
+                hori_score = hori_pixels.sum() / 4
+
+                edge_score = (vert_score ** 2 + hori_score ** 2) ** 0.5
+                edge_img[row, col] = [edge_score] * 3
+
+        edge_img = edge_img / edge_img.max()
+
+        return edge_img
+
--- HandTracking/main.py
+++ HandTracking/main.py
@@ -1,15 +1,21 @@
 import cv2
 import numpy as np
 from HandTracking.CameraHandler import *
+from HandTracking.Modules.SobelEdge import *
 
 cap = cv2.VideoCapture(0)
 
-cameraHandler = CameraHandler(cap)
+# cameraHandler = CameraHandler(cap)
+# frames = cameraHandler.grabFrame()
+
+frame = cv2.imread('a.jpg', cv2.IMREAD_COLOR)
+sobelEdge = SobelEdge(frame)
+edges = sobelEdge.detectionLoop()
 
 
-cameraHandler.grabFrame()
-
-
+cv2.imshow('edge', edges)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 
 # When everything done, release the capture
 cap.release()
