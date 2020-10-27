a2355dbe7cdd650ee62047d5e206766e316b50d2
Mads <Mdwp19@student.aau.dk>
2020-10-27 16:07:15 +0100

    Queue Data Strukture
    It works, apparently xD

--- HandTracking/CameraImgList.py
+++ HandTracking/CameraImgList.py
@@ -1,34 +1,62 @@
-Import Node
 
-class CameraImgList:
+class Node:
+    def __init__(self, imagedata):
+        self.imagedata = imagedata
+        self.nextimg = None
+
+
+#Frontimg for finding the image in the front.
+class CameraQueue:
     def __init__(self):
-        self.headval = None
+        self.frontimg = None
 
-list1 = CameraImgList()
-list1.headval = Node("Test")
+
+    #Throw the shit in the back - method | Queue Data Structure.
+    def QueuingNode(self, newpicture):
+
+        NewImage = Node(newpicture)
+        if self.frontimg is None:
+            self.frontimg = NewImage
+            return
+
+        last = self.frontimg
+        while (last.nextimg): #Loops through the structure till it's at the last link.
+            last = last.nextimg
+        last.nextimg = NewImage #adds the image to the chain.
+
+
+    #Funktion that remove the first element.
+    def DeQueingNode(self):
+        if self.frontimg is None:
+            return None
+        else:
+            self.frontimg = self.frontimg.nextimg
+
+
+# SLET RESTEN INDEN AFLEVERING.
+
+#PrintListen Funktion
+def listprint(self):
+    printval = self.frontimg
+    while printval is not None:
+        print(printval.imagedata)
+        printval = printval.nextimg
+
+list1 = CameraQueue()
+list1.frontimg = Node("Test")
 e2 = Node("Test2")
 e3 = Node("Test3")
 
 #Linking first node to second node.
-list1.headval.nextval = e2
+list1.frontimg.nextimg = e2
 
 #Linking second Node to third node
-e2.nextval = e3
+e2.nextimg = e3
 
-def listprint(self):
-    printval = self.headval
-    while printval is not None:
-        print(printval.dataval)
-        printval = printval.nextval
 
-def QueuingNode(self, newpicture):
-    NewNode = Node(newpicture)
-    if self.headval is None:
-        self.headval = NewNode
-        return
-    laste = self.headval
-    while(laste.nextval):
-        laste = laste.nextval
-    laste.nextval=NewNode
+CameraQueue.DeQueingNode(list1)
+listprint(list1)
 
+
+#def RemovingFirstNode(self):
 
