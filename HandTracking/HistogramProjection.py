f41075de5f657d47ae3f6e29b4239b729653475b
Sebastian <sebber280@live.dk>
2020-11-09 13:22:07 +0100

    Histrogram can detect B + added binarized pics for testing

--- HandTracking/HistogramProjection.py
+++ HandTracking/HistogramProjection.py
@@ -64,12 +64,12 @@ class HistogramProjection:
         return sumColsX
 
     def trimZeros(self, list):
-        trimmed = []
+        trimmedvert = []
         for i in range(0, len(list)):
             if list[i] != 0:
-                trimmed.append(list[i])
+                trimmedvert.append(list[i])
 
-        return trimmed
+        return trimmedvert
 
     def checkDuplicates(self, list):
         checked = []
@@ -84,30 +84,10 @@ class HistogramProjection:
         # print('Duplicates:', duplicates)
         return checked
 
-    def checkRelativePos(self, list):
-        groups = []  ##Group number : Height
-        gNumber = 0
-
-        print('Groups pre-loop', groups)
-
-        for z in range(len(list)):
-
-            if list[z][1] < list[z + 1 < len(list)][
-                1] + 10:  #### Revise +10 alt efter hvor godt det kommer til at fungere
-                print('Groups first ifloop', groups)
-                groups.append([list[z][0], gNumber])
-
-            elif list[z][1] > list[z + 1 < len(list)][1] + 10:
-                gNumber += 1
-                print('Groups second ifloop', groups)
-                groups.append([list[z][0], gNumber])
-
-        return groups
-
     def getTopXPercentile(self, list, percentile):
         toppunkter = []
         for i in range(len(list)):
-            if list[i] > max(list) * int(percentile / 100):
+            if list[i] > max(list) * percentile / 100:
                 toppunkter.append([list[i], list.index(list[i])])
                 # print('Top 90% values:', list[i])
                 # print('-------------------------------------')
@@ -117,6 +97,7 @@ class HistogramProjection:
         sortToppunkter = self.checkDuplicates(toppunkter)
 
         return sortToppunkter
+
 
     def checkIfA(self, listVert, listHori):
         trimVert = self.trimZeros(listVert)
@@ -136,78 +117,104 @@ class HistogramProjection:
         print('Placement of Median:', trimVert.index(trimVert[int(len(trimVert) / 2)]))
         print('Median:', trimVert[int(len(trimVert) / 2)])
         print('----------------------------------------------------')
-        print('Median of Vertical distribution:', trimHori[int(len(trimHori) / 2)])
+        print('Median of Horizontal distribution:', trimHori[int(len(trimHori) / 2)])
         print('Placement of Median:', trimHori.index(trimHori[int(len(trimHori) / 2)]))
         print('Median:', trimHori[int(len(trimHori) / 2)])
         medianHori = trimHori[int(len(trimHori) / 2)]
         print('----------------------------------------------------')
-        maxHeightRelation = max(trimVert) / max(trimHori)
-        print('Forhold mellem største højde i de to histrogrammer', maxHeightRelation)
+
 
         print('Mean value of vertA:', np.mean(trimVert))
-        print('70% of Mean:', np.mean(trimVert) * 0.70)
-        mean70p = np.mean(trimVert) * 0.70
+        print('70% of Mean:', np.mean(trimVert) * 0.75)
+        mean75p = np.mean(trimVert) * 0.75
         print('value 10% in', trimVert[int(len(trimVert) * 0.10)])
 
-
-        for s in range(int(len(trimVert) * 0.10), len(trimVert)): ###Denne parameter er gældene for alle tre håndtegn så den skal laves om
-            if trimVert[s] < mean70p:
-                isA = False
-
-            else:
-                isA = True
-
-        if isA == True:
+        maxHeightRelation = max(trimVert) / max(trimHori)
+        print('Forhold mellem største højde i de to histrogrammer', maxHeightRelation)
+        if 0.35 < maxHeightRelation < 0.45:
             aScore += 1
-            print('bScore from steady course throughout vertical distribution')
+            print('aScore from relation between max heights of the two distributions')
 
-        if medianHori > max(trimHori) * 0.80:
+
+        if medianHori > max(trimHori) * 0.98:
             aScore += 1
-            print('bScore from median being in the top 90%')
+            print('aScore from median being in the top 98%')
 
         if 1.7 < lengRelation < 2.2:
             aScore += 1
-            print('bScore from relationship between vertical and horizontal length')
+            print('aScore from relationship between vertical and horizontal length')
 
-        print('bScore:', aScore)
+        print('aScore:', aScore)
         return aScore
 
-    def checkIfC(self, list):
+    def checkifB(self, listvert, listhori):
+        trimmedVert = self.trimZeros(listvert)
+        trimmedHori = self.trimZeros(listhori)
+        bScore = 0
+
+        lenVert = len(trimmedVert)
+        lenHori = len(trimmedHori)
+
+        print('lenVert:', lenVert)
+        print('lenHori:', lenHori)
+
+        print('biggest val / len(hori):', max(trimmedHori) / len(trimmedHori))
+
+        print('biggest val vert:', max(trimmedVert))
+        print('biggest val vert place:', trimmedVert.index(max(trimmedVert)))
+        if trimmedVert.index(max(trimmedVert)) < len(trimmedVert) * 0.25:
+            bScore += 1
+            print('bScore from toppoint being placed in first 25% of distribution')
+
+        topLenRelation = max(trimmedHori) / len(
+            trimmedHori)  ###Relation between the toppoint and the length of the horizontal distribution
+        if 400 < topLenRelation < 440:
+            bScore += 1
+            print('bScore from relation between the toppoint and the length of the horizontal distribution')
+
+        print('top90 cluster', self.getTopXPercentile(trimmedHori, 85))
+        print('len top90 cluster', len(self.getTopXPercentile(trimmedHori, 85)))
+        top85cluster = self.getTopXPercentile(trimmedHori, 85)
+        lenTop85cluster = len(top85cluster)
+
+        if 25 < lenTop85cluster < 35:
+            bScore += 1
+            print('bScore from large cluster of toppoints')
+
+
+        print('bScore:', bScore)
+        return bScore
+
+    def checkIfC(self, listvert, listhori):
         #### Karaktaristika for C;
         # - To toppunkter af relativ højde til hinanden
         # - Toppunkter af relativ højde til de omkringliggende
         # - Toppunkter er af relativ afstand til hinanden
 
-        trimmed = self.trimZeros(list)  # Removing 0's
+        trimmedvert = self.trimZeros(listvert)  # Removing 0's
+        trimmedhori = self.trimZeros(listhori)
         cScore = 0
 
-        # print('first 30%', len(trimmed) * 0.30)
-        # print('first 40%', len(trimmed) * 0.40)
-        # print('first 50%', len(trimmed) * 0.50)
-        # print('first 80%', len(trimmed) * 0.80)
-        # print('Listen', len(list))
-        # print('Trimmed', len(trimmed))
+        for i in range(0, int(len(trimmedvert) * 0.30)):
+            # print('Første 30%:', trimmedvertvert[i])
+            if trimmedvert[i] == max(trimmedvert[0:int(len(trimmedvert) * 0.30)]):
+                print('toppunktplacement:', trimmedvert.index(trimmedvert[i]))
+                lilleTop = [trimmedvert.index(trimmedvert[i]), trimmedvert[i]]
 
-        for i in range(0, int(len(trimmed) * 0.30)):
-            # print('Første 30%:', trimmed[i])
-            if trimmed[i] == max(trimmed[0:int(len(trimmed) * 0.30)]):
-                print('toppunktplacement:', trimmed.index(trimmed[i]))
-                lilleTop = [trimmed.index(trimmed[i]), trimmed[i]]
-
-        for i in range(int(len(trimmed) * 0.30), len(trimmed)):
-            # print('Sidste 70%:', trimmed[i])
-            if trimmed[i] == max(trimmed[int(len(trimmed) * 0.30):len(trimmed)]):
-                print('toppunktplacement:', trimmed.index(trimmed[i]))
-                storTop = [trimmed.index(trimmed[i]), trimmed[i]]
+        for i in range(int(len(trimmedvert) * 0.30), len(trimmedvert)):
+            # print('Sidste 70%:', trimmedvert[i])
+            if trimmedvert[i] == max(trimmedvert[int(len(trimmedvert) * 0.30):len(trimmedvert)]):
+                print('toppunktplacement:', trimmedvert.index(trimmedvert[i]))
+                storTop = [trimmedvert.index(trimmedvert[i]), trimmedvert[i]]
 
         dstTops = storTop[0] - lilleTop[0]
-        dstTopsnLengthRelation = dstTops / len(trimmed)
+        dstTopsnLengthRelation = dstTops / len(trimmedvert)
 
         print('Afstand mellem toppunkter:', dstTops)
         print('Forhold mellem afstand og samlede længde:', dstTopsnLengthRelation)
 
         heightDiffRelation = storTop[1] / lilleTop[1]
-        heightLengthRelation = storTop[1] / len(trimmed)
+        heightLengthRelation = storTop[1] / len(trimmedvert)
 
         print('Forhold mellem højde:', heightDiffRelation)
         print('Forhold mellem største højde ift. længde', heightLengthRelation)
@@ -224,6 +231,20 @@ class HistogramProjection:
             cScore += 1
             print('cScore from heightLengthRelation')
 
+        print('first 33%', len(trimmedhori) * 0.33)
+        print("33% value", trimmedhori[int(len(trimmedhori) * 0.33)])
+        val33 = trimmedhori[int(len(trimmedhori) * 0.33)]
+        print('first 50%', len(trimmedhori) * 0.50)
+        print(("50% value", trimmedhori[int(len(trimmedhori) * 0.50)]))
+        val50 = trimmedhori[int(len(trimmedhori) * 0.50)]
+        print('first 66%', len(trimmedhori) * 0.66)
+        print(("66% value", trimmedhori[int(len(trimmedhori) * 0.66)]))
+        val66 = trimmedhori[int(len(trimmedhori) * 0.66)]
+
+        if val33 + val33*0.33 < val50 < val66:
+            cScore += 1
+            print('cscore for increase in right intervals')
+
         print('cScore:', cScore)
         return cScore
 
@@ -233,23 +254,30 @@ if __name__ == '__main__':
     imgC = cv2.imread('binaryC.png')
     imgA = cv2.imread('binary.png')
 
-    ProjectDist = HistogramProjection(imgC)
-    horizontalDistC = ProjectDist.getHistogram_HProjection()
-    verticalDistC = ProjectDist.getHistogram_VProjection()
+    ProjectDistC = HistogramProjection(imgC)
+    horizontalDistC = ProjectDistC.getHistogram_HProjection()
+    verticalDistC = ProjectDistC.getHistogram_VProjection()
+    ProjectDistC.checkIfA(verticalDistC, horizontalDistC)
+    ProjectDistC.checkifB(verticalDistC, horizontalDistC)
+    ProjectDistC.checkIfC(verticalDistC, horizontalDistC)
 
+    # ProjectDistB = HistogramProjection(imgB)
+    # verticalB = ProjectDistB.getHistogram_VProjection()
+    # horizB = ProjectDistB.getHistogram_HProjection()
+    # ProjectDistB.checkIfA(verticalB, horizB)
+    # ProjectDistB.checkifB(verticalB, horizB)
+    # ProjectDistB.checkIfC(verticalB, horizB)
 
-    ProjectDistB = HistogramProjection(imgB)
-    verticalB = ProjectDistB.getHistogram_HProjection()
-    horizB = ProjectDistB.getHistogram_VProjection()
+    # ProjectDistA = HistogramProjection(imgA)
+    # vertA = ProjectDistA.getHistogram_VProjection()
+    # horizA = ProjectDistA.getHistogram_HProjection()
+    # ProjectDistA.checkIfA(vertA, horizA)
+    # # ProjectDistA.checkifB(vertA, horizA)
+    # # ProjectDistA.checkIfC(vertA, horizA)
 
-
-    ProjectDistA = HistogramProjection(imgA)
-    vertA = ProjectDistA.getHistogram_VProjection()
-    horizA = ProjectDistA.getHistogram_HProjection()
-    
 
     # cv2.imshow('C', imgC)
-    cv2.imshow('A', imgA)
+    # cv2.imshow('A', imgA)
     # cv2.imshow('B', imgB)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
