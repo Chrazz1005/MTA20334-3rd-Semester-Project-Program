0effb42a316a06e54f7bcc6805cacc99bff90b27
tsta19 <tsta19@student.aau.dk>
2020-11-16 09:44:33 +0100

    Thresholding is Done

--- HandTracking/Thresholding.py
+++ HandTracking/Thresholding.py
@@ -4,47 +4,56 @@ import time
 
 
 class Thresholding:
-    # ------------ Image Definitions ------------
-
-    # ------------ Count Program Execution Time ------------
     startTime = time.time()
 
-    def splitImage(self, image):
-        r, g, b = cv2.split(image)
+    # x, y = (image > limit).nonzero()
+    # vals = image[x, y]
 
-        return r  # Since the image is "binary", being either 0 in all channels or 255, it doesn't matter which channel is returned
+    def convertToHSV(self, image):
+        for x, row in enumerate(image):
+            for y, col in enumerate(row):
+                red = col[2] / 255
+                green = col[1] / 255
+                blue = col[0] / 255
+                maximum = max(red, green, blue)
+                minimum = min(red, green, blue)
+                delta = maximum - minimum
 
-    # ------------ Binary Function ------------
-    def binarize(self, image):
-        outputImage = np.zeros(image.shape, dtype=image.dtype)
-        # For every row in the input image we receive the y coordinate and the row
-        print('starting binary loop...')
-        for y, row in enumerate(image):
-            # For every pixel in row we receive the x coordinate and the pixel value
-            for x, pixel in enumerate(row):
-                # Initializing the color value R, G, B
-                H, S, V = pixel[0], pixel[1], pixel[2]
-                if H in range(35, 115) and S in range(115, 240) and V in range(0, 200):
-                    # H in range(0, 90) and S in range(40, 195) and V in range(20, 170):
-                    # H in range(0, 80) and S in range(0, 195) and V in range(25, 170):
-                    # If so, turn the pixel white
-                    outputImage[y, x] = 255
+                hue = 0
+                if delta == 0:
+                    pass
+                elif maximum == red:
+                    hue = (60 * (((green - blue) / delta) % 6)) % 360
+                elif maximum == green:
+                    hue = (60 * ((blue - red) / delta + 2)) % 360
+                elif maximum == blue:
+                    hue = (60 * ((red - green) / delta + 4)) % 360
+
+                value = maximum * 255
+
+                if maximum == 0:
+                    saturation = 0
                 else:
-                    # If not, turn the pixel black
-                    outputImage[y, x] = 0
+                    saturation = delta / maximum * 255
 
-        print('binary pic is now binary yes')
-        outputImage = self.splitImage(outputImage)
-        return outputImage
+                image[x, y] = [hue / 2, round(saturation), round(value)]
 
+        return image
 
-if __name__ == '__main__':
-    inputImage = cv2.imread("Pictures/Green_Hand_One_W_Background.png", cv2.COLOR_BGR2HSV)
-    thresh = Thresholding()
+    def binarize(self, image):
+        # self.convertToHSV(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        npArray = np.array(np.asarray(image))
 
-    binary = thresh.binarize(inputImage)
-    cv2.imshow("Hand Spread", inputImage)
-    cv2.imshow("Hand Spread Binary", binary)
+        Range = [(30, 80), (0, 255), (0, 255)]
+        redColorRange = np.logical_and(Range[0][0] < npArray[:, :, 0], npArray[:, :, 0] < Range[0][1])
+        greenColorRange = np.logical_and(Range[1][0] < npArray[:, :, 1], npArray[:, :, 1] < Range[1][1])
+        blueColorRange = np.logical_and(Range[2][0] < npArray[:, :, 2], npArray[:, :, 2] < Range[2][1])
+        rgbRange = (redColorRange * greenColorRange * blueColorRange)
 
-    print("Program Execution Time: %s" % round(time.time() - thresh.startTime), "seconds")
-    cv2.waitKey(0)
+        npArray[rgbRange] = 255
+        npArray[np.logical_not(rgbRange)] = 0
+
+        return npArray
+
+    print("Program Execution Time: %s" % (time.time() - startTime), "seconds")
