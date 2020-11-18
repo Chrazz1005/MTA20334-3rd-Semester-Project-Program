a343074b4bfc9c7725b4c886a2f6e909f1c3051a
tsta19 <tsta19@student.aau.dk>
2020-11-18 13:25:14 +0100

    Fixed multiple call error

--- HandTracking/AspectRatio.py
+++ HandTracking/AspectRatio.py
@@ -3,6 +3,9 @@ from GeometryCalculator import *
 
 class AspectRatio:
     handGesture = ""
+    height = 0
+    width = 0
+    areal = 0
 
     def __init__(self, image):
         self.image = image
@@ -10,23 +13,22 @@ class AspectRatio:
     def calculateArea(self):
         gc = GeometryCalculator(self.image)
         gc.cPoints()
-        height = (gc.wMaxY - gc.wMinY) + 1
-        width = (gc.wMaxX - gc.wMinX) + 1
-        areal = (height * width)
-        return areal, height, width
+        self.height = (gc.wMaxY - gc.wMinY) + 1
+        self.width = (gc.wMaxX - gc.wMinX) + 1
+        self.areal = (self.height * self.width)
 
     def calculateAspectRatio(self):
-        aspectRatio = (self.calculateArea()[1] / self.calculateArea()[2])
+        aspectRatio = (self.height / self.width)
         return round(aspectRatio, 2)
 
     def compareAspectRatio(self):
         handGesture = ""
         if 1.7 < self.calculateAspectRatio() < 2.0 or 2.0 < self.calculateAspectRatio() < 1.7:
-            handGesture = "A"
+            handGesture = "Gesture_A"
         elif 2.1 < self.calculateAspectRatio() < 2.5 or 2.5 < self.calculateAspectRatio() < 2.1:
-            handGesture = "B"
+            handGesture = "Gesture_B"
         elif 0.6 < self.calculateAspectRatio() < 1.3 or 1.3 < self.calculateAspectRatio() < 0.6:
-            handGesture = "C"
+            handGesture = "Gesture_C"
         else:
             print("No Hand Gesture Detected.")
 
