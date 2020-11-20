b7420ffe8fc0dbb2308609ff6c427bc4cc87b896
tsta19 <tsta19@student.aau.dk>
2020-11-20 14:05:22 +0100

    Connected database to web

--- HandTracking/Database.py
+++ HandTracking/Database.py
@@ -1,21 +1,33 @@
+import pymongo
 import pyodbc
 import time
 
 
 class Database:
-    database = pyodbc.connect('Driver={SQL Server};'
-                              'Server=DESKTOP-RBC5RA1\SQLEXPRESS;'
-                              'Database=hand_gesture_dataset;'
-                              'Trusted_Connection=yes;')
-
-    sqlTable = "hand_gesture_dataset.dbo.gesture_dataset"
-    sqlTableClean = "gesture_dataset"
-
     # ----------- How to use -----------
     # addToTable: Will take 1-3 inputs which will be added to the database
     # deleteEntireDatabase: Deletes the entire database (this is non-revertible)
     # resetIdentityField: Resets the id to start from 1
     # showDatabase: Shows the database in the console
+
+    def __init__(self):
+        userName = input("DB Username: ")
+        userPassword = input("DB Password: ")
+        try:
+            print("-------- Connection --------")
+            print("Name:", userName)
+            print("Password:", userPassword)
+            print("----------------------------")
+            print("Attempting to connect database..")
+            self.client = pymongo.MongoClient(
+                "mongodb+srv://" + userName + ":" + userPassword + "@cluster0.jqbtc.mongodb.net/3rd_semester_project?retryWrites=true"
+                                                                   "&w=majority")
+            print("Connection established.")
+        except:
+            print("Connection failed.")
+
+        self.database = self.client["hand_gesture_data"]
+        self.databaseCollection = self.database["3rd_semester_project"]
 
     def addToTable(self, x1=0, x2=0, x3=0):
         cursor = self.database.cursor()
@@ -24,6 +36,14 @@ class Database:
         print("Inserted the following into the database")
         print("You inserted the following into the database:")
         print(x1, x2, x3)
+
+    def mdbAdd(self, x1=0, x2=0, x3=0):
+        dataToInsert = {
+            "A": x1,
+            "B": x2,
+            "C": x3
+        }
+        self.databaseCollection.insert_one(dataToInsert)
 
     def deleteEntireDatabase(self):
         cursor = self.database.cursor()
