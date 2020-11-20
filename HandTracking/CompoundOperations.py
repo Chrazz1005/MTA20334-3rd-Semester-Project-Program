6b4422f80b9e2b5a8762e16980d3b822dfedd1ec
Sebastian <sebber280@live.dk>
2020-11-20 10:07:20 +0100

    Compound operations

---
+++ HandTracking/CompoundOperations.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2
+
+
+class CompoundOperations:
+    def __init__(self, img, method):
+        self.img = img
+        dilutedArray = np.zeros(img.shape, dtype=np.uint8)
+
+        # if method is dilution:
+        #     self.method = dilution(img)
+
+        if method is erosion:
+            for x in range(img.shape[1] - 1):
+                for y in range(img.shape[0] - 1):
+                    dilutedArray[y, x] = erosion(img, y, x)
+            return dilutedArray
+
+
+
+# Hardcoding a cross-kernel 3x3 (1 in top, 3 in middle, 1 at bottom.)
+
+def erosion(img, y=None, x=None):
+    if img[y, x].all() == 0: #.any() because we have 3 channels that are 0's.
+        return 0
+    img_Height = img.shape[0]
+    img_Width = img.shape[1]
+    position = img[y,x] # y,x
+    if [position[0], position[1] - 1] == 1 or x>=0: # y,x-1
+        if [position[0], position[1] + 1] == 1 or x<=img_Width: # y,x+1
+            if [position[0] + 1, position[1]] == 1 or x<=img_Height: # y+1,x
+                if [position[0] - 1, position[1]] == 1 or x>=0: # y-1,x
+                    return 255
+    return 0
+
+
+#Test-Picture:
+test = cv2.imread("binaryC.png")
+
+#
+# #Creating an array with 0's in the size of the picture.
+# dilutedArray = np.zeros(test.shape,dtype=np.uint8)
+
+#The loop that goes through the picture:
+# for x in range(test.shape[1]-1):
+#     for y in range(test.shape[0]-1):
+#         dilutedArray[y,x] = erosion(test, y, x)
+
+dilutedArray = CompoundOperations(test,erosion(test))
+
+#For "testing" to see how well it works:
+cv2.imshow("yay", dilutedArray)
+cv2.waitKey(0)
+print(test[0,0])
