56e1247982b66ad0dad0b82c671f6e17c1d631a9
Mads <Mdwp19@student.aau.dk>
2020-11-23 11:14:33 +0100

    Added dilation

--- HandTracking/CompoundOperations.py
+++ HandTracking/CompoundOperations.py
@@ -14,19 +14,29 @@ class CompoundOperations:
     def erode(self):
         img = self.img
         dilutedArray = self.dilutedArray
-        print(dilutedArray.shape)
         for x in range(img.shape[1] - 1):
             #print("x in range", x)
             for y in range(img.shape[0] - 1):
-                print(y)
                 dilutedArray[y, x] = erosion(img, y, x)
+        return dilutedArray
+
+    def dilate(self):
+        img = self.img
+        dilutedArray = self.dilutedArray
+        for x in range(img.shape[1] - 1):
+            for y in range(img.shape[0] - 1):
+                if img[y,x] == 255:
+                    dilutedArray[y, x] = 255
+                    dilutedArray[y, x - 1] = 255
+                    dilutedArray[y - 1, x] = 255
+                    dilutedArray[y, x + 1] = 255
+                    dilutedArray[y + 1, x] = 255
+                    print(img[y,x])
+                    print(dilutedArray[y,x])
         return dilutedArray
 
     def get_image(self):
         return self.dilutedArray
-
-
-
 
 # Hardcoding a cross-kernel 3x3 (1 in top, 3 in middle, 1 at bottom.)
 
@@ -60,33 +70,3 @@ def erosion(img, y, x):
     #                 return 255
     # return 0
 
-
-#Test-Picture:
-# arraytest = np.array([[
-# testarray = np.array([  0,   0,   0], [  0,   0,   0], [  0,   0,   0], [  0,   0,   0], [  0,   0,   0], [255, 255, 255],
-#                      [255, 255, 255], [255, 255, 255], [255, 255, 255],
-#  [  0   0   0])
-#
-# test = cv2.imread("binaryC.png")
-# print(test[200])
-
-
-#
-# #Creating an array with 0's in the size of the picture.
-# dilutedArray = np.zeros(test.shape,dtype=np.uint8)
-
-#The loop that goes through the picture:
-# for x in range(test.shape[1]-1):
-#     for y in range(test.shape[0]-1):
-#         dilutedArray[y,x] = erosion(test, y, x)
-
-# CV = CompoundOperations(arraytest)
-# for x in range(10):
-#     CompoundOperations.erode(CV)
-# Fuck = CompoundOperations.get_image(CV)
-#For "testing" to see how well it works:
-
-
-# cv2.imshow("yay",Fuck)
-# cv2.waitKey(0)
-# print(test[0,0])
