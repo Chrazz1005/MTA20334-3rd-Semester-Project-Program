e652120fd3efaad39b2a2a6970a6a1903454ea1c
Sebastian <sebber280@live.dk>
2020-11-27 15:43:09 +0100

    std, avg for B, C

--- HandTracking/EuclideanDistance.py
+++ HandTracking/EuclideanDistance.py
@@ -18,6 +18,15 @@ class EuclideanDistance:
     ar_Amax = ar_Aavg + ar_Astd
     ar_Amin = ar_Aavg - ar_Astd
 
+    ar_Bstd = 0.04313208
+    ar_Bavg = 0.001211328
+    ar_Bmin = ar_Bavg - ar_Bstd
+    ar_Bmax = ar_Bavg + ar_Bstd
+
+    ar_Cstd = 0.04622898
+    ar_Cavg = 0.001144468
+    ar_Cmin = ar_Cavg - ar_Cstd
+    ar_Cmax = ar_Cavg + ar_Cstd
 
     compactness_A_min = 73
     compactness_A_max = 81
@@ -31,6 +40,16 @@ class EuclideanDistance:
     c_Amax = c_Aavg + c_Astd
     c_Amin = c_Aavg - c_Astd
 
+    c_Bstd = 0.002937374
+    c_Bavg = 0.04212978
+    c_Bmin = c_Bavg - c_Bstd
+    c_Bmax = c_Bavg + c_Bstd
+
+    c_Cstd = 0.006012833
+    c_Cavg = 0.045000879
+    c_Cmin = c_Cavg - c_Cstd
+    c_Cmax = c_Cavg + c_Cstd
+
     heightRelation_A_min = 0.60
     heightRelation_A_max = 0.80
     heightRelation_B_min = 0.35
@@ -42,6 +61,17 @@ class EuclideanDistance:
     hr_Aavg = 0.0007752173
     hr_Amax = hr_Aavg + hr_Astd
     hr_Amin = hr_Aavg - hr_Astd
+
+    hr_Bstd = 0.04409414
+    hr_Bavg = 0.0002984198
+    hr_Bmin = hr_Bavg - hr_Bstd
+    hr_Bmax = hr_Bavg + hr_Bstd
+
+    hr_Cstd = 0.04664601
+    hr_Cavg = 0.0007490136
+    hr_Cmin = hr_Cavg - hr_Cstd
+    hr_Cmax = hr_Cavg + hr_Cstd
+
 
     verticalRatio_A_min = 130
     verticalRatio_A_max = 160
@@ -55,6 +85,15 @@ class EuclideanDistance:
     vertihr_Amin = vertihr_Aavg - vertihr_Astd
     vertihr_Amax = vertihr_Aavg + vertihr_Astd
 
+    vertihr_Bstd = 0.034432279
+    vertihr_Bavg = 0.07424375
+    vertihr_Bmin = vertihr_Bavg - vertihr_Bstd
+    vertihr_Bmax = vertihr_Bavg + vertihr_Bstd
+
+    vertihr_Cstd = 0.1305096
+    vertihr_Cavg = 0.1649563
+    vertihr_Cmin = vertihr_Cavg - vertihr_Cstd
+    vertihr_Cmax = vertihr_Cavg + vertihr_Cstd
 
     horizontalRatio_A_min = 280
     horizontalRatio_A_max = 310
@@ -68,6 +107,16 @@ class EuclideanDistance:
     horihr_Amin = horihr_Aavg - horihr_Astd
     horihr_Amax = horihr_Aavg + horihr_Astd
 
+    horihr_Bstd = 0.2768627
+    horihr_Bavg = 0.30209657
+    horihr_Bmin = horihr_Bavg - horihr_Bstd
+    horihr_Bmax = horihr_Bavg + horihr_Bstd
+
+    horihr_Cstd = 0.2321599
+    horihr_Cavg = 0.25892256
+    horihr_Cmin = horihr_Cavg - horihr_Cstd
+    horihr_Cmax = horihr_Cavg + horihr_Cstd
+
     localMaximum_A_min = 0.8
     localMaximum_A_max = 1.1
     localMaximum_B_min = 1.1
@@ -79,6 +128,16 @@ class EuclideanDistance:
     lmaxdiff_Aavg = 0.0009187231
     lmaxdiff_Amin = lmaxdiff_Aavg - lmaxdiff_Astd
     lmaxdiff_Amax = lmaxdiff_Aavg + lmaxdiff_Astd
+
+    lmaxdiff_Bstd = 0.04359541
+    lmaxdiff_Bavg = 0.0007715659
+    lmaxdiff_Bmin = lmaxdiff_Bavg - lmaxdiff_Bstd
+    lmaxdiff_Bmax = lmaxdiff_Bavg + lmaxdiff_Bstd
+
+    lmaxdiff_Cstd = 0.04568783
+    lmaxdiff_Cavg = 0.001658877
+    lmaxdiff_Cmin = lmaxdiff_Cavg - lmaxdiff_Cstd
+    lmaxdiff_Cmax = lmaxdiff_Cavg + lmaxdiff_Cstd
 
 
     A_min = np.array((aspectRatio_A_min, compactness_A_min, heightRelation_A_min, verticalRatio_A_min,
