a2deaba347438800240557ca2d5f11577c393bcc
tsta19 <tsta19@student.aau.dk>
2020-11-27 13:42:52 +0100

    Updated color finder

--- HandTracking/ColorFinder.py
+++ HandTracking/ColorFinder.py
@@ -10,15 +10,7 @@ class ColorFinder:
     def findAverageColor(self):
         convertedImage = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
         npArray = np.array(np.asarray(convertedImage))
-        for y, row in enumerate(npArray):
-            for x, pixel in enumerate(row):
-                H, S, V = pixel[2], pixel[1], pixel[0]
-                if H in range(40, 70) and S in range(0, 255) and V in range(0, 255):
-                    npArray[y, x] = H, S, V
-                    print("FOUND")
-                else:
-                    npArray[y, x] = 0, 0, 0
-                    print("NOPE")
+        npArray[:, :, 0], npArray[:, :, 1], npArray[:, :, 2] = np.average(npArray, axis=(0, 1))
         return npArray
 
 
