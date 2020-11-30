63909b239c2b0d762f3fb2c10f0b161a01d47f6d
tsta19 <tsta19@student.aau.dk>
2020-11-30 11:37:34 +0100

    Updated ColorFinder (Done)

--- HandTracking/ColorFinder.py
+++ HandTracking/ColorFinder.py
@@ -14,36 +14,13 @@ class ColorFinder:
         # convertedImage = cv2.cvtColor(npArray, cv2.COLOR_BGR2HSV)
         return npArray
 
+    def Average(self, length):
+        avg = sum(length) / len(length)
+        return avg
 
-image = cv2.imread("B_ONLY_HAND/B7.png")
 
-# A1: 75, 48, 77
-# A2: 86, 49, 75
-# A3: 91, 38, 74
-# A4: 90, 46, 76
-# A5: 91, 39, 60
-# A6: 76, 26, 67
-# A7: 105, 35, 69
-# A8: 85, 33, 72
-# A9: 74, 34, 64
-# A10: 76, 29, 58
-# A11: 68, 43, 63
-
-# B1: 75, 49, 78
-# B2: 106, 32, 76
-# B3: 88, 34, 73
-# B4: 88, 43, 78
-# B5: 92, 33, 67
-# B6: 74, 23, 74
-# B7: 104, 31, 77
-# B8:
-# B9:
-# B10:
-# B11:
-
+image = cv2.imread("C_ONLY_HAND/C11.png")
 
 cf = ColorFinder(image)
 converted = cf.findAverageColor()
-cv2.imshow("Average Color", converted)
-cv2.waitKey(0)
-print(converted)
+print(cf.Average(list))
